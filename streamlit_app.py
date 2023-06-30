ee5749b9fca15c8bac1be9fd0b7632bcf6b0b547
SwatGetmann <swatgetmann@gmail.com>
2023-06-30 16:36:44 +0400

    filtering

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,5 +16,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits", options=list(fruit_list.index))
-streamlit.dataframe(fruit_list)
+fruits_selected = streamlit.multiselect("Pick some fruits", 
+                      options=list(fruit_list.index), 
+                      default=['Avocado', 'Strawberries'])
+streamlit.dataframe(fruit_list.loc[fruits_selected])
