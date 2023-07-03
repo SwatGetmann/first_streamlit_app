bd3ccb80655eaf5b62b5816fb18973be2c011796
SwatGetmann <swatgetmann@gmail.com>
2023-07-03 13:00:19 +0400

    function move

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,15 +24,19 @@ fruits_selected = streamlit.multiselect("Pick some fruits",
                       default=['Avocado', 'Strawberries'])
 streamlit.dataframe(fruit_list.loc[fruits_selected])
 
+def get_fruitvice_data(fruit):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+
 streamlit.header("Fruityvice Fruit Advice!")
 try: 
     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        streamlit.dataframe(get_fruitvice_data(fruit_choice))
 except URLError as e:
     streamlit.error()
 
